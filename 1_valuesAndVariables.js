c36c8500bfabbd26cd2e131464ac49952f6b485f
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-30 18:33:39 +0530

    1 Values And Variables

---
+++ 1_valuesAndVariables.js
@@ -0,0 +1,11 @@
+var myName = "Deepak";
+var myAge = 26
+var grade, std = 2
+console.log(myName, myAge, grade, std);
+
+// Challenge-1: Idenify Valid variable
+// var _myName = "Deepak"
+// var _1my__Name = "Bhai"
+// var 1myName = "Bhai"
+// var $myName = "DeepakBabu"
+// var myName% = "Deepak"
