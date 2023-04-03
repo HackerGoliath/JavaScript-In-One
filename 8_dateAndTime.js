991cdda9de73cdc2c4b201cccde3e90297a83295
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-04-03 18:57:10 +0530

    7 strings

---
+++ 8_dateAndTime.js
@@ -0,0 +1,6 @@
+// There are 4 ways o create date object
+// new Date()
+// new Date(year, month, day, hours, minutes, seconds, milliseconds) // takes 7 args
+// We can not avoid month section
+// new Date(date string)
+
