2239ba7dc751e7dd31536d5f5934677ba723e84c
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-04-04 17:47:08 +0530

    9 Math Object

---
+++ 9_mathObject.js
@@ -0,0 +1,58 @@
+// Math Object help us to perform mathematical tasks on numbers
+// console.log(Math.PI);
+
+// Math.round(): returns the value rounded to its nearest integer
+// let num = 10.34436674
+// num = 10.54436674
+// console.log(Math.round(num));
+
+// Math.pow(x,y): returns the value x to the power of y
+// console.log(Math.pow(3, 4));
+// console.log(3 ** 4);
+
+// Math.sqrt(x) = returns the square root of x
+// console.log(Math.sqrt(81));
+// console.log(Math.sqrt(66));
+
+// Math.abs(x): returns the absolute(positive) value of x
+// console.log(Math.abs(-19));
+// console.log(Math.abs(-10.9));
+// console.log(Math.abs(-10 + 9));
+
+// Math.ceil(x): returns the rounded value of x to its upper integer
+// console.log(Math.ceil(12.745));
+// console.log(Math.round(12.745));
+// console.log(Math.ceil(99.0145));
+// console.log(Math.round(99.0145));
+
+// Math.floor(x): returns the rounded value of x to its lowest integer
+// console.log(Math.floor(12.1745));
+// console.log(Math.round(12.1745));
+// console.log(Math.floor(99.9901));
+// console.log(Math.round(99.9901));
+
+// Math.min(): used to find the lowest number among args
+// console.log(Math.min(23, 12, 3, -4, 6.09, 17));
+
+// Math.max():  used to find the highest number among args
+// console.log(Math.max(23, 12, 3, 4, -6.09, 17));
+
+// Math.random(): returns a random number between 0(inclusive) and 1(exclusive)
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log(Math.floor(Math.random() * 10)); // 0 to 9
+
+// Math.round(): rounded to nearest integer
+
+// Math.trunc(): returns the integer part
+// console.log(Math.trunc(12.56));
+// console.log(Math.trunc(-12.96));
+
+// Practice: if the args is positive, Math.trunc() is equivalent to Math.floor(),
+// otherwise Math.trunc() is equivalent to Math.ceil()
+// console.log(Math.trunc(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.trunc(-4.6));
+// console.log(Math.ceil(-4.6));
+// console.log(Math.floor(-4.6));
