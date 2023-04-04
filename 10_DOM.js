e3bb17a364b2d9ad3622372d5b528f192119cc58
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-04-04 19:13:27 +0530

    10 DOM started

---
+++ 10_DOM.js
@@ -0,0 +1,38 @@
+// Window vs document
+// Window contains url but document can not
+// Window conatins document and screen and other objects but document can not have screen object
+// alert(location.href)
+// if (confirm("Want to visit fb?")) {
+//     location.href = "https://facebook.com"
+// }
+
+// document returns whole document
+// Know root element of document -> html is the root element of the document
+// document.documentElement
+
+// reference to domNavigation.html
+// get child nodes(count space,tab as text)
+// document.body.childNodes
+
+// only elements
+// document.body.children
+// document.body.children.length
+
+// Whwther the document has child nodes
+// document.body.childNodes
+
+// get parent of element(parent of body and head is html)
+// document.body.parentNode
+// document.head.parentNode
+
+//Siblings
+// body and head are siblings
+// document.body.nextElementSibling
+// document.body.previousElementSibling
+
+// get scripts collection
+// document.scripts
+// ---------------------------------
+
+// How to search the elements and references
+// refer to search.html
