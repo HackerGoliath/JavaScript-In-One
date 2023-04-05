428436e46cfae6934d38abdfff158cc62664ea71
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-04-05 19:24:31 +0530

    11 Events started

---
+++ 11_events.js
@@ -0,0 +1,28 @@
+/*
+1. 4 ways of wrtiting Events:
+i. using alert()
+ii. By calling a function
+iii. using inline events (HTML onclick="" property and element.onclick="")
+iv. using Event Listener (addEventListener  and IE's attachEvent)
+Refer to eventTypes.html in events folder
+----------------------------------------------------------------------
+
+2. Event Object: Event Object is the parent of event objects means all the events are under it
+eg: MouseEvent, focusEvent, KeyboardEvent
+refer to eventObj.html in events folder
+-------------------------------------------------------------
+
+3. MouseEvent: Events that occur when the mouse interacts with the html documents
+refer to mouseDownUp.html in events folder
+------------------------------------------------------------------
+
+4. KeyboardEvent: It occurs when user presses a key on keyboard
+refer to keyPress.html in events folder
+--------------------------------------------------------------------
+
+5. InputEvents: 
+The onchange event occurs when the value of an element has been changed.
+For radio buttons and checkboxes, the onchange event occurs when the 
+checked state has been changed 
+refer to onChange.html in events folder
+*/
